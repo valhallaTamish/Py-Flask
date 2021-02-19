b805fa9ee6a7cc4c0c323b61dcf4c24a338d107c
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-02-19 06:56:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def hello():
     return render_template("index.html")
-if __name__=='__main__':
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
